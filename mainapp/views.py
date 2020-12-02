d3e9982753d3a7bf73e5c4407ab5efac68db813b
Tippman <gl.8700@hmail.com>
2020-12-03 00:27:33 +0300

    hw_lesson_2

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html',context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'Каталог',
+        'products_list': [
+            {'href': 'products_new', 'name': 'Новинки'},
+            {'href': 'products_dress', 'name': 'Одежда'},
+            {'href': 'products_shoes', 'name': 'Обувь'},
+            {'href': 'products_accessories', 'name': 'Аксессуары'},
+            {'href': 'products_presents', 'name': 'Подарки'},
+        ],
+    }
+    return render(request, 'mainapp/products.html',context)
