293570a02b864bec9575d590d16ef5cec0ad2c13
Tippman <gl.8700@hmail.com>
2021-01-16 15:23:12 +0300

    ДЗ лекция 1. Контекстный процессор прописал, но не указывал в шаблонах, пока не знаю на что его добавить, в дальнейшем думаю найду применение

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -44,6 +44,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'mainapp.context_processor.basket'
             ],
         },
     },
@@ -101,3 +102,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'authapp.User'
 
 LOGIN_URL = '/auth/login/'
+
+DOMAIN = 'http://localhost:9000'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+EMAIL_HOST_USER = 'django@geekshop.local'
+EMAIL_HOST_PASSWORD = 'danik'
+EMAIL_USE_SSL = False
+
+# EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/email-messages'
