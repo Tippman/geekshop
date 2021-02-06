3b6292f324fc5f260fb4c646b657190a485a0c39
Tippman <gl.8700@hmail.com>
2021-02-06 15:26:32 +0300

    L7_cached products by category

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.core.cache import cache
 
 
-def get_links_menu():
+def get_links_category():
     if settings.LOW_CACHE:
         key = 'links_menu'
         links_menu = cache.get(key)
@@ -15,6 +15,21 @@ def get_links_menu():
         return links_menu
     else:
         return Category.objects.filter(is_active=True)
+
+
+def get_products_by_category_ordered_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_by_category_ordered_by_price_{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category_id=pk, is_active=True,
+                                              category__is_active=True).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category_id=pk, is_active=True,
+                                      category__is_active=True).select_related('category')
+
 
 def index(request):
     context = {
@@ -34,7 +49,7 @@ class Products(ListView):
         context.update({
             'title': 'Каталог',
             # 'categories': Category.objects.filter(is_active=True),
-            'categories': get_links_menu(),
+            'categories': get_links_category(),
         })
         return context
 
@@ -54,12 +69,13 @@ class ProductsByCategory(ListView):
         context.update({
             'title': Category.objects.get(pk=self.kwargs['category_id']),
             # 'categories': Category.objects.filter(is_active=True),
-            'categories': get_links_menu(),
+            'categories': get_links_category(),
         })
         return context
 
     def get_queryset(self):
-        return Product.objects.filter(category_id=self.kwargs['category_id'], is_active=True,
-                                      category__is_active=True).select_related('category')
+        return get_products_by_category_ordered_by_price(self.kwargs['category_id'])
+        # return Product.objects.filter(category_id=self.kwargs['category_id'], is_active=True,
+        #                               category__is_active=True).select_related('category')
         # return Product.objects.filter(category_id=self.kwargs['category_id'], is_active=True,
         #                               category__is_active=True)
